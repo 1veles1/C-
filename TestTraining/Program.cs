8c99bb02095cb7fe92081f62c833fbd87ad1bcac
1veles1 <veles.rod.1984@gmail.com>
2016-07-23 11:38:46 +0600

    Добавьте файлы проекта.

---
+++ TestTraining/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
