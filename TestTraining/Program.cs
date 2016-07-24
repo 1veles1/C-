bedfee4e8b418ce6aa8909d1768bbd7840a99214
1veles1 <veles.rod.1984@gmail.com>
2016-07-24 15:27:10 +0600

    Применение поразрядного оператора исключающее ИЛИ.

--- TestTraining/Program.cs
+++ TestTraining/Program.cs
@@ -1198,7 +1198,7 @@ namespace TestTraining
         }
     }
 }
-*/
+
 
 // Применить поразрядный оператор ИЛИ, чтобы сделать число нечетным.
 
@@ -1222,6 +1222,48 @@ namespace TestTraining
                 Console.WriteLine(" num после установки младшего разряда = " + num + " \n ");
                 Console.WriteLine();
                 }
+            Console.ReadKey();
+        }
+    }
+}
+*/
+
+// Применение поразрядного оператора исключающее ИЛИ.
+
+using System;
+
+namespace TestTraining
+{
+    class Encode
+    {
+        static void Main()
+        {
+            char ch1 = 'H';
+            char ch2 = 'i';
+            char ch3 = '!';
+            int key = 88;
+
+            Console.WriteLine(" Исходное сообщение = " + ch1 + ch2 + ch3);
+            Console.WriteLine();
+
+            // Зашифровать сообщение.
+
+            ch1 = (char)(ch1 ^ key);
+            ch2 = (char)(ch2 ^ key);
+            ch3 = (char)(ch3 ^ key);
+
+            Console.WriteLine(" Зашифрованное сообщение = " + ch1 + ch2 + ch3);
+            Console.WriteLine();
+
+            // Расшифровать сообщение.
+
+            ch1 = (char)(ch1 ^ key);
+            ch2 = (char)(ch2 ^ key);
+            ch3 = (char)(ch3 ^ key);
+
+            Console.WriteLine(" Расшифрованное сообщение = " + ch1 + ch2 + ch3);
+            Console.WriteLine();
+
             Console.ReadKey();
         }
     }
