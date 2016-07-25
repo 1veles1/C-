2d89b433d7e5af65a681b84d11cea7e1811cd3d8
1veles1 <veles.rod.1984@gmail.com>
2016-07-25 19:17:32 +0600

    Кнопки

--- WinFormApp/Form1.cs
+++ WinFormApp/Form1.cs
@@ -16,5 +16,30 @@ namespace WinFormApp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1.Text);
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(" This is my first programm ", " Info ");
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
     }
 }
