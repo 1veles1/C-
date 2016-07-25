788dc1adf7c7e0bdd0a6ecdcf92c7c0df1c00b30
1veles1 <veles.rod.1984@gmail.com>
2016-07-25 12:57:19 +0600

    Применение операторов сдвига.

--- TestTraining/Test.cs
+++ TestTraining/Test.cs
@@ -1304,3 +1304,48 @@ namespace TestTraining
 }
 */
 
+// Применение операторов сдвига.
+
+using System;
+
+namespace Test
+{
+    class ShiftDemo
+    {
+        static void Main()
+        {
+            int val = 1;
+
+                for (int i = 0; i < 8; i++)
+                {
+                for (int t = 128; t > 0; t = t / 2)
+                {
+                    if ((val & t) != 0) Console.Write(" 1 ");
+                    if ((val & t) == 0) Console.Write(" 0 ");
+                }
+                Console.WriteLine(" Сдвиг влево ");
+                Console.WriteLine();
+
+                val = val << 1; // сдвиг влево
+                }
+                Console.WriteLine();
+
+                val = 128;
+
+                for (int i = 0; i < 8; i++)
+                {
+                for (int t = 128; t > 0; t = t / 2)
+                {
+                    if ((val & t) != 0) Console.Write(" 1 ");
+                    if ((val & t) == 0) Console.Write(" 0 ");
+
+                }
+                Console.WriteLine(" Сдвиг вправо ");
+                Console.WriteLine();
+
+                val = val >> 1; // сдвиг вправо
+                }
+            Console.ReadKey();
+        }
+    }
+}
