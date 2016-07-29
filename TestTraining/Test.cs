2a45532d0cd359259586870472200d1c0ca4bfe4
1veles1 <veles.rod.1984@gmail.com>
2016-07-29 16:48:25 +0600

    Добавить метод, принимающий два аргумента.

--- TestTraining/Test.cs
+++ TestTraining/Test.cs
@@ -1065,7 +1065,7 @@ namespace Test
 }
 */
 // Простой пример применения параметра.
-
+/*
 using System;
 
 namespace Test
@@ -1103,6 +1103,81 @@ namespace Test
             Console.Clear();
             Console.Beep();
 
+        }
+    }
+}
+*/
+// Добавить метод, принимающий два аргумента.
+using System;
+
+namespace Test
+{
+    class ChkNum
+    {
+        // Возвратить значение true, если значение параметра х окажется простым числом.
+
+        public bool IsPrime(int x)
+        {
+            if (x <= 1) return false;
+
+            for (int i = 2; i <= x / i; i++)
+                if ((x % i) == 0)
+                    return false;
+            return true;
+        }
+        
+        // Возвратить наименьший общий множитель.
+        public int LeastComFactor(int a, int b)
+        {
+            int max;
+
+            if (IsPrime(a) || IsPrime(b))
+                return 1;
+
+            max = a < b ? a : b;
+
+            for (int i = 2; i <= max / 2; i++)
+                if (((a % i) == 0) && ((b % i) == 0))
+                    return i;
+            return 1;
+        }
+    }
+    class ParmDemo
+    {
+        static void Main()
+        {
+            ChkNum ob = new ChkNum();
+
+            int a, b;
+
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+            for (int i = 2; i < 10; i++)
+                if (ob.IsPrime(i))
+                    Console.WriteLine(" " + i + " простое число" + "\n");
+                else Console.WriteLine(" " + i + " непростое число." + "\n");
+
+            a = 7;
+            b = 8;
+
+            Console.WriteLine(" Наименьший общий множитель чисел " + a + " и " + b + " = " + ob.LeastComFactor(a, b) + "\n");
+
+            a = 100;
+            b = 8;
+
+            Console.WriteLine(" Наименьший общий множитель чисел " + a + " и " + b + " = " + ob.LeastComFactor(a, b) + "\n");
+
+            a = 100;
+            b = 75;
+
+            Console.WriteLine(" Наименьший общий множитель чисел " + a + " и " + b + " = " + ob.LeastComFactor(a, b) + "\n");
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" Done ");
+            Console.ReadLine();
+            Console.Clear();
+            Console.Beep();
         }
     }
 }
