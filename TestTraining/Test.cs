cc22b33a4ba4c2dabf8e33a7f86859a312ad5492
1veles1 <veles.rod.1984@gmail.com>
2016-07-29 15:51:17 +0600

    Возвратить значение из метода AreaPerPerson().

--- TestTraining/Test.cs
+++ TestTraining/Test.cs
@@ -927,7 +927,7 @@ namespace Test
 }
 */
 // Добавить метод в класс Building.
-
+/*
 using System;
 
 namespace Test
@@ -988,4 +988,81 @@ namespace Test
         }
     }
 }
+*/
+// Возвратить значение из метода AreaPerPerson().
 
+using System;
+
+namespace Test
+{
+    class Building
+    {
+        public int Floors; // количество этажей
+        public int Area; // общая площадь здания
+        public int Occupants; // количество жильцов
+
+        // Возвратить величину площади на одного человека,
+
+        public int AreaPerPerson()
+        {
+            return Area / Occupants;
+        }
+    }
+    // Использовать значение, возвращаемое методом AreaPerPerson!).
+    class BuildingDemo
+    {
+        static void Main()
+        {
+            Building house = new Building();
+            Building office = new Building();
+
+            int areaPP; // площадь на одного человека
+
+            // Присвоить значения полям в объекте house.
+
+            house.Occupants = 4;
+            house.Area = 2500;
+            house.Floors = 2;
+
+            // Присвоить значения полям в объекте office.
+
+            office.Occupants = 25;
+            office.Area = 4200;
+            office.Floors = 3;
+
+            // Получить площадь на одного человека в жилом доме.
+
+            areaPP = house.AreaPerPerson(); // При использовании закоментированого способа эта строка не нужна !!!
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Дом имеет =  " + house.Floors + " этажа " + house.Occupants + " жильца " + house.Area +
+                                " кв. футов общей площади, из них " + "\n" + areaPP + " приходится на одного человека");
+
+            // Console.WriteLine("Дом имеет =  " + house.Floors + " этажа " + house.Occupants + " жильца " + house.Area +
+            // " кв. футов общей площади, из них " + house.AreaPerPerson() + " приходится на одного человека");
+
+            Console.WriteLine();
+
+            // Получить площадь на одного человека в учреждении.
+
+            areaPP = office.AreaPerPerson(); // При использовании закоментированого способа эта строка не нужна !!!
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Учреждение имеет = " + office.Floors + " этажа " + office.Occupants + " работников " +
+                                office.Area + " кв. футов общей площади, из них" + "\n" + areaPP + " приходится на одного человека");
+
+            // Console.WriteLine("Учреждение имеет = " + office.Floors + " этажа " + office.Occupants + " работников " +
+            //                   office.Area + " кв. футов общей площади, из них" + house.AreaPerPerson() + " приходится на одного человека");
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" Done ");
+            Console.ReadLine();
+            Console.Clear();
+            Console.Beep();
+
+        }
+    }
+}
+
+ 
