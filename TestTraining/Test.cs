f7437432bf7c9b31efc521e69c492095a78cd998
1veles1 <veles.rod.1984@gmail.com>
2016-07-30 10:34:17 +0600

    Добавить параметризированный метод, вычисляющий максимальное количество человек, которые могут занимать здание, исходя из заданной минимальной площади на одного человека.

--- TestTraining/Test.cs
+++ TestTraining/Test.cs
@@ -1108,6 +1108,7 @@ namespace Test
 }
 */
 // Добавить метод, принимающий два аргумента.
+/*
 using System;
 
 namespace Test
@@ -1171,6 +1172,70 @@ namespace Test
             b = 75;
 
             Console.WriteLine(" Наименьший общий множитель чисел " + a + " и " + b + " = " + ob.LeastComFactor(a, b) + "\n");
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" Done ");
+            Console.ReadLine();
+            Console.Clear();
+            Console.Beep();
+        }
+    }
+}
+*/
+// Добавить параметризированный метод, вычисляющий максимальное количество человек, которые могут
+// занимать здание, исходя из заданной минимальной площади на одного человека.
+
+using System;
+
+namespace Test
+{
+    class Building
+    {
+        public int Floors; // количество этажей
+        public int Area; // общая площадь здания
+        public int Occupants; // количество жильцов
+
+        // Возвратить площадь на одного человека.
+        public int AreaPerPerson()
+        {
+            return Area / Occupants;
+        }
+
+        // Возвратить максимальное количество человек, занимающих здание,
+        // исходя из заданной минимальной площади на одного человека.
+
+        public int MaxOccupant(int minArea)
+        {
+            return Area / minArea;
+        }
+    }
+    // Использовать метод MaxOccupant().
+    class BuildingDemo
+    {
+        static void Main()
+        {
+            Building house = new Building();
+            Building office = new Building();
+
+            // Присвоить значения полям в объекте house.
+
+            house.Occupants = 4;
+            house.Area = 2500;
+            house.Floors = 2;
+
+            // Присвоить значения полям в объекте office.
+
+            office.Occupants = 25;
+            office.Area = 4200;
+            office.Floors = 3;
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(" Максимальное количество человек в доме " + " если на каждого должно приходиться " +
+                                300 + " кв. футов = " + house.MaxOccupant(300) + "\n");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Максимальное количество человек " + "в учреждении, " + "если на каждого должно приходиться " +
+                                300 + " кв. футов = " + office.MaxOccupant(300) + "\n");
 
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.Red;
