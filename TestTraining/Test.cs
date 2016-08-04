2ebbab05a3d36407234e8b339813780c9741587b
1veles1 <veles.rod.1984@gmail.com>
2016-08-04 16:16:19 +0600

    Некоторые операции со строками.

--- TestTraining/Test.cs
+++ TestTraining/Test.cs
@@ -2176,7 +2176,7 @@ namespace Test
 	}
 */
 // Поиск в массиве с помощью оператора цикла foreach.
-
+/*
 using System;
 
 namespace Test
@@ -2226,3 +2226,105 @@ namespace Test
 		}
 		
 	}
+*/
+//  Создать и вывести символьную строку.
+/*
+using System;
+
+namespace Test
+	{
+	class StringDemo
+		{
+		static void Main()
+			{
+
+			char[] charray = { 'S', 'T', 'R', 'I', 'N', 'G' };
+
+			string str1 = new string(charray);
+			string str2 = " Еще одна строка ";
+
+			Console.WriteLine(str1);
+			Console.WriteLine(str2);
+			}
+		}
+	}
+*/
+
+// Некоторые операции со строками.
+
+using System;
+using System.Globalization;
+
+namespace Test
+	{
+	class StrOps
+		{
+		static void Main()
+			{
+			String str1 = "Программировать в .NET лучше всего на С#.";
+			String str2 = "Программировать в .NET лучше всего на С#.";
+			String str3 = "Строки в C# весьма эффективны.";
+			String strUp, strLow;
+			Int32 result, idx;
+
+			Console.WriteLine("str1 = " + str1);
+			Console.WriteLine("Длина строки str1 = " + str1.Length);
+
+			// Создать варианты строки str1, набранные
+			// прописными и строчными буквами.
+
+			strLow = str1.ToLower(CultureInfo.CurrentCulture);
+			strUp = str1.ToUpper(CultureInfo.CurrentCulture);
+
+			Console.WriteLine("Вариант строки str1, набранный строчными буквами" + strLow);
+			Console.WriteLine("Вариант строки str1,набранный прописными буквами" + strUp);
+
+			Console.WriteLine();
+
+			// Вывести строку str1 посимвольно.
+
+			Console.WriteLine("Вывод строки str1 посимвольно.");
+			for(int i = 0; i < str1.Length; i++)
+				Console.Write(str1[i]);
+
+			Console.WriteLine("\n");
+
+			// Сравнить строки способом порядкового сравнения.
+
+			if(str1 == str2)
+				Console.WriteLine("str1 == str2");
+			else
+				Console.WriteLine("str1 != str2");
+			if(str1 == str3)
+				Console.WriteLine("str1 == str3");
+			else
+				Console.WriteLine("str1 != str3");
+
+			// Сравнить строки с учетом культурной среды.
+
+			result = string.Compare(str3, str1, StringComparison.CurrentCulture);
+
+			if(result == 0)
+				Console.WriteLine("Строки str1 и str3 равны");
+			else if(result < 0)
+				Console.WriteLine("Строка str1 меньше строки str3");
+			else
+				Console.WriteLine("Строка str1 больше строки str3");
+
+			Console.WriteLine();
+
+			// Присвоить новую строку переменной str2.
+
+			str2 = "Один Два Три Один";
+
+			// Поиск подстроки.
+
+			idx = str2.IndexOf("Один", StringComparison.Ordinal);
+			Console.WriteLine("Индекс первого вхождения подстроки <Один> = " + idx);
+
+			idx = str2.LastIndexOf("Один", StringComparison.Ordinal);
+			Console.WriteLine("Индекс последнего вхождения подстроки <Один> = " + idx);
+			}
+		}
+	}
+
