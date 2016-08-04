936455562a982705526ea4d995bf6b887f1655b9
1veles1 <veles.rod.1984@gmail.com>
2016-08-04 11:45:33 +0600

    Продемонстрировать применение свойства Length при обращении со ступенчатыми массивами

--- TestTraining/Test.cs
+++ TestTraining/Test.cs
@@ -1949,7 +1949,7 @@ static void Main() {
 }
 */
 // Поменять местами содержимое элементов массива.
-
+/*
 using System;
 
 namespace Test
@@ -1993,7 +1993,7 @@ namespace Test
 					nums2[j] = nums1[i];
 
 			Console.Write( " Содержимое массива в обратном порядке = " );
-
+			
 			for ( i = 0; i < nums2.Length; i++ )
 				Console.Write( nums2[i] + " " );
 			Console.WriteLine();
@@ -2016,3 +2016,41 @@ namespace Test
 			}
 		}
 	}
+*/
+// Продемонстрировать применение свойства Length при обращении со ступенчатыми массивами.
+
+using System;
+
+namespace Test
+	{
+	class Jagged
+		{
+		static void Main ()
+			{
+			int[][] network_nodes = new int[4][];
+			network_nodes[0] = new int[3];
+			network_nodes[1] = new int[7];
+			network_nodes[2] = new int[2];
+			network_nodes[3] = new int[5];
+			int i, j;
+			// Сфабриковать данные об использовании ЦП.
+			for ( i = 0; i < network_nodes.Length; i++ )
+				for ( j = 0; j < network_nodes[i].Length; j++ )
+					network_nodes[i][j] = i * j + 70;
+			Console.WriteLine( "Общее количество узлов сети: " +
+			network_nodes.Length + "\n" );
+			for ( i = 0; i < network_nodes.Length; i++ )
+				{
+				for ( j = 0; j < network_nodes[i].Length; j++ )
+					{
+					Console.Write( "Использование в узле сети " + i +
+					" ЦП " + j + ": " );
+					Console.Write( network_nodes[i][j] + "% " );
+					Console.WriteLine();
+					}
+				Console.WriteLine();
+				}
+			}
+		}
+	}
+	
