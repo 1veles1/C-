06db166d56bd5b22f479b74d114eda17b8e41c91
1veles1 <veles.rod.1984@gmail.com>
2016-08-06 16:03:57 +0600

    Возвратить массив из метода.

---
+++ ReturnArrayFromMethod/Program.cs
@@ -0,0 +1,55 @@
+﻿
+// Возвратить массив из метода.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturnArrayFromMethod
+	{
+	class Factor
+		{
+
+		// Метод возвращает массив facts, содержащий множители аргумента num.
+		// При возврате из метода параметр numfactors типа out будет содержать
+		// количество обнаруженных множителей.
+
+		public Int32[] FindFactors(Int32 num, out Int32 numfactors)
+			{
+			Int32[] facts = new Int32[80]; // размер массива 80 выбран произвольно
+			Int32 i, j;
+
+			// Найти множители и поместить их в массив facts.
+			
+			for(i = 2, j = 0; i < num / 2 + 1; i++)
+				if((num % i) == 0)
+					{
+					facts[j] = i;
+					j++;
+					}
+			numfactors = j;
+			return facts;
+			}
+		}
+	class FindFactors
+		{
+		static void Main()
+			{
+			Factor f = new Factor();
+			Int32 numfactors;
+			Int32[] factors;
+
+			factors = f.FindFactors(1000, out numfactors);
+
+			Console.WriteLine(" Множители числа 1000 = ");
+
+			for(Int32 i = 0; i < numfactors; i++)
+				Console.WriteLine(factors[i] + " ");
+
+			Console.WriteLine();
+			}
+		}
+	}
+	
---
+++ ReturnArrayFromMethod/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ReturnArrayFromMethod")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("ReturnArrayFromMethod")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("2c32e327-597e-4b03-b12d-3c9f48fc9a53")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
