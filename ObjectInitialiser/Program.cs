204d115b1536efbc0f35c37ce5b8851432726034
1veles1 <veles.rod.1984@gmail.com>
2016-08-07 10:56:36 +0600

    применение инициализаторов объектов

---
+++ ObjectInitialiser/Program.cs
@@ -0,0 +1,29 @@
+﻿// Простой пример, демонстрирующий применение инициализаторов объектов.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectInitialiser
+	{
+	class MyClass
+		{
+		public Int32 Count;
+		public String Str;
+		}
+
+	class ObjInitDemo
+		{
+		static void Main()
+			{
+
+			// Сконструировать объект типа MyClass, используя инициализаторы объектов.
+
+			MyClass obj = new MyClass { Count = 100, Str = "Test"};
+
+			Console.WriteLine(obj.Count + " " + obj.Str);
+			}
+		}
+	}
