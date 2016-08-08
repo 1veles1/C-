bc87d848abd879b4f39d4900eb6a028b0b5ca7e8
1veles1 <veles.rod.1984@gmail.com>
2016-08-08 15:05:37 +0600

    применение статического класса

---
+++ StaticClass/Program.cs
@@ -0,0 +1,66 @@
+﻿// Продемонстрировать применение статического класса.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClass
+	{
+	static class NumericFn
+		{
+
+		// Возвратить обратное числовое значение.
+
+		static public Double Reciprocal(Double num)
+			{
+			return 1 / num;
+			}
+
+		// Возвратить дробную часть числового значения.
+
+		static public Double FracPart(Double num)
+			{
+			return num - (Int32) num;
+			}
+		
+		// Возвратить логическое значение true, если числовое
+		// значение переменной num окажется четным.
+
+		static public Boolean IsEven(Double num)
+			{
+			return (num % 2) == 0 ? true : false; 
+			}
+
+		// Возвратить логическое значение true, если числовое
+		// значение переменной num окажется нечетным.
+
+		static public Boolean IsOdd(Double num)
+			{
+			return !IsEven(num);
+			}
+		}
+	class StaticClassDemo
+		{
+		static void Main()
+			{
+			Console.WriteLine(" Обратная величина числа 5 = " + NumericFn.Reciprocal(5));
+			Console.WriteLine(" Дробная часть числа 4.234 = " + NumericFn.FracPart(4.234));
+
+			if(NumericFn.IsEven(10))
+				 Console.WriteLine(" 10 — четное число ");
+
+			if(NumericFn.IsOdd(5))
+				 Console.WriteLine(" 5 — нечетное число ");
+
+
+			// Далее следует попытка создать экземпляр объекта класса NumericFn,
+			// что может стать причиной появления ошибки.
+			
+			// NumericFn ob = new NumericFn(); // Ошибка!
+			}
+		}
+
+	}
+	
---
+++ StaticConstractor/Program.cs
@@ -0,0 +1,41 @@
+﻿// Применить статический конструктор.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticConstractor
+	{
+	class Cons
+		{
+		public static Int32 alpha;
+		public Int32 beta;
+
+		// Статический конструктор.
+
+		static Cons()
+			{
+			alpha = 99;
+			Console.WriteLine(" В статическом конструкторе ");
+			}
+		
+		// Конструктор экземпляра.
+
+		public Cons()
+			{
+			beta = 100;
+			Console.WriteLine(" В конструкторе экземпляра ");
+			}
+		}
+	class ConsDemo
+		{
+		static void Main()
+			{
+			Cons ob = new Cons();
+			Console.WriteLine(" Cons.alpha = " + Cons.alpha);
+			Console.WriteLine(" ob.beta = " + ob.beta);
+			}
+		}
+	}
