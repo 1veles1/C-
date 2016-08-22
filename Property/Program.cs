3763d42f1be2b6508d66b8f311af6954f2317ad7
1veles1 <veles.rod.1984@gmail.com>
2016-08-22 14:43:36 +0600

    Еще о свойствах

--- Property/Program.cs
+++ Property/Program.cs
@@ -167,3 +167,112 @@ namespace Property
 	}
 */
 // Превратить переменную ErrFlag в свойство.
+
+using System;
+
+namespace Property
+	{
+	class FailSoftArray
+		{
+		Int32[] a;                      // ссылка на базовый массив
+		Int32 len;                      // длина массива
+		Boolean ErrFlag;                // теперь это частная переменная, обозначающая результат последней операции
+
+		// Построить массив заданного размера.
+
+		public FailSoftArray(Int32 size)
+			{
+			a = new Int32[size];
+			len = size;
+			}
+
+		// Свойство Length только для чтения.
+
+		public Int32 Lenght
+			{
+			get
+				{
+				return
+					len;
+				}
+			}
+
+		// Свойство Error только для чтения.
+
+		public Boolean Error
+			{
+			get
+				{
+				return
+					ErrFlag;
+				}
+			}
+
+		// Это индексатор для класса FailSoftArray.
+
+		public Int32 this[Int32 index]
+			{
+			// Это аксессор get.
+			get
+				{
+
+				if(ok(index))
+					{
+					ErrFlag = false;
+					return 
+						a[index];
+					}
+				else
+					{
+					ErrFlag = true;
+					return 
+						0;
+					}
+				}
+			
+			// Это аксессор set.
+			set
+				{
+
+				if(ok(index))
+					{
+					a[index] = value;
+					ErrFlag = false;
+					}
+				else
+					ErrFlag = true;
+				}
+			}
+
+				// Возвратить логическое значение true, если индекс находится в установленных границах.
+
+		private Boolean ok(Int32 index)
+			{
+			if(index >= 0 & index < Lenght)
+				return 
+					true;
+			return 
+				false;
+			}
+		}
+
+			// Продемонстрировать применение отказоустойчивого массива.
+
+	class FinalFSDemo
+		{
+		static void Main()
+			{
+			FailSoftArray fs = new FailSoftArray(5);
+
+			// Использовать свойство Error.
+
+			for(Int32 i = 0; i < fs.Lenght + 1; i++)
+				{
+				fs[i] = i * 10;
+
+				if(fs.Error)
+					Console.WriteLine(" Ошибка в индексе " + i);
+				}
+			}
+		}
+	}
