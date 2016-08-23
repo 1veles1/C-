4b3d7deee0629e5873bf2d371ccf573c97dbd1e3
1veles1 <veles.rod.1984@gmail.com>
2016-08-23 21:48:10 +0600

    Protected Modifier

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Класс для двумерных объектов (Пример простой иерархии классов)
-
+/*
 using System;
 
 
@@ -97,5 +97,150 @@ namespace Inheritance
 			}
 		}
 	}
+*/
+// Доступ к закрытым членам класса не наследуется. Этот пример кода не подлежит компиляции.
+/*
+using System;
 
-	
+		// Класс для двумерных объектов.
+class TwoDShape
+	{
+	double Width;	// теперь это закрытая переменная
+	double Height;	// теперь это закрытая переменная
+	public void ShowDim()
+		{
+		Console.WriteLine("Ширина и высота равны " +
+		Width + " и " + Height);
+		}
+	}
+		// Класс Triangle, производный от класса TwoDShape.
+class Triangle: TwoDShape
+	{
+	public string Style; // тип треугольника
+						 // Возвратить площадь треугольника.
+	public double Area()
+		{
+		return 
+			Width * Height / 2;		// Ошибка, доступ к закрытому
+									// члену класса запрещен
+		}
+		// Показать тип треугольника.
+	public void ShowStyle()
+		{
+		Console.WriteLine("Треугольник " + Style);
+		}
+	}
+*/
+// Использовать открытые свойства для установки и получения значений закрытых членов класса.
+/*
+using System;
+
+namespace Inheritance
+	{
+
+	// Класс для двумерных объектов.
+	class TwoDshape
+		{
+		Double pri_width;	// теперь это закрытая переменная
+		Double pri_height;  // теперь это закрытая переменная
+
+		// Свойства ширины и высоты двумерного объекта.
+
+		public Double Width
+			{
+			get
+				{
+				return
+					pri_width;
+				}
+			set
+				{
+				pri_width = value < 0 ? -value : value;
+				}
+			}
+		public Double Height
+			{
+			get
+				{
+				return
+					pri_height;
+				}
+			set
+				{
+				pri_height = value < 0 ? -value : value;
+				}
+			}
+		public void ShowDim()
+			{
+			Console.WriteLine(" Ширина и высота = " + Width + " и " + Height);
+			}
+		}
+		// Класс для треугольников, производный от класса TwoDShape.
+
+	class Triangle: TwoDshape
+		{
+		public String Style; // тип треугольника
+
+		// Возвратить площадь треугольника.
+
+		public Double Area()
+			{
+			return
+				Width * Height / 2;
+			}
+
+		// Показать тип треугольника.
+
+		public void ShowStyle()
+			{
+			Console.WriteLine(" Треугольник " + Style);
+			}
+		}
+
+	class Shapes2
+		{
+		static void Main()
+			{
+			Triangle t_1 = new Triangle();
+			Triangle t_2 = new Triangle();
+
+			t_1.Width = 4.0;
+			t_1.Height = 4.0;
+			t_1.Style = " равнобедренный ";
+
+			t_2.Width = 8.0;
+			t_2.Height = 12.0;
+			t_2.Style = " прямоугольный ";
+
+			Console.WriteLine(" Сведения об объекте t_1 = ");
+
+			t_1.ShowStyle();
+			t_1.ShowDim();
+
+			Console.WriteLine(" Площадь = " + t_1.Area());
+
+			Console.WriteLine();
+
+			Console.WriteLine(" Сведения об объекте t_2 = ");
+
+			t_2.ShowStyle();
+			t_2.ShowDim();
+
+			Console.WriteLine(" Площадь = " + t_2.Area());
+			}
+		}
+	}
+*/
+// Добавить конструктор в класс Triangle.
+
+using System;
+
+namespace Inheritance
+	{
+
+	// Класс для двумерных объектов.
+	class TwoDShape
+		{
+
+		}
+	}
---
+++ ProtectedModifier/Program.cs
@@ -0,0 +1,49 @@
+﻿// Продемонстрировать применение модификатора доступа protected.
+
+using System;
+
+
+namespace ProtectedModifier
+	{
+	class B
+		{
+		protected Int32 i, j; // члены, закрытые для класса В, но доступные для класса D 
+
+		public void Set(Int32 a, Int32 b)
+			{
+			i = a;
+			j = b;
+			}
+		public void Show()
+			{
+			Console.WriteLine(i + " " + j);
+			}
+		}
+	class D: B
+		{
+		Int32 k; //  закрытый член
+
+		// члены i и j класса В доступны для класса D
+
+		public void Setk()
+			{
+			k = i * j;
+			}
+		public void Showk()
+			{
+			Console.WriteLine(k);
+			}
+		}
+	class ProtectedDemo
+		{
+		static void Main()
+			{
+			D ob = new D();
+
+			ob.Set(2, 3);	// допустимо, поскольку доступно для класса D
+			ob.Show();		// допустимо, поскольку доступно для класса D
+			ob.Setk();		// допустимо, поскольку входит в класс D
+			ob.Showk();		// допустимо, поскольку входит в класс D
+			}
+		}
+	}
