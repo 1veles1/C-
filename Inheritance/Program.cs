61cab7d5add43bfb76cc44af5ac00f5e7a8d5885
1veles1 <veles.rod.1984@gmail.com>
2016-08-24 11:46:32 +0600

    Наследование (base)

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -232,7 +232,7 @@ namespace Inheritance
 	}
 */
 // Добавить конструктор в класс Triangle.
-
+/*
 using System;
 
 namespace Inheritance
@@ -241,6 +241,334 @@ namespace Inheritance
 	// Класс для двумерных объектов.
 	class TwoDShape
 		{
+		Double pri_width;
+		Double pri_height;
 
+		// Свойства ширины и длины объекта.
+
+		public Double Width
+			{
+			get
+				{
+				return
+					pri_width;
+				}
+			set
+				{
+				pri_width = value < 0 ? -value : value;
+				}
+			}
+		public Double Height
+			{
+			get
+				{
+				return
+					pri_height;
+				}
+			set
+				{
+				pri_height = value < 0 ? -value : value;
+				}
+			}
+		public void ShowDim()
+			{
+			Console.WriteLine(" Ширина и длина = " + Width + " и " + Height);
+			}
+		}
+
+	// Класс для треугольников, производный от класса TwoDShape.
+
+	class Triangle: TwoDShape
+		{
+		String Style;
+
+		//  Конструктор.
+
+		public Triangle(String s, Double w, Double h)
+			{
+			Width = w;  // инициализировать член базового класса	
+			Height = h; // инициализировать член базового класса
+			Style = s;  // инициализировать член производного класса
+			}
+
+		// Возвратить площадь треугольника.
+
+		public Double Area()
+			{
+			return
+				Width * Height / 2;
+			}
+
+		// Показать тип треугольника.
+
+		public void ShowStyle()
+			{
+			Console.WriteLine(" Треугольник " + Style);
+			}
+		}
+	class Shapes3
+		{
+		static void Main()
+			{
+			Triangle tr_1 = new Triangle(" равнобедренный ", 4.0, 4.0);
+			Triangle tr_2 = new Triangle(" прямоугольный ", 8.0, 12.0);
+
+			Console.WriteLine(" Сведения об объекте tr_1 = ");
+
+			tr_1.ShowStyle();
+			tr_1.ShowDim();
+
+			Console.WriteLine(" Площадь = " + tr_1.Area());
+
+			Console.WriteLine();
+
+			Console.WriteLine(" Сведения об объекте tr_2 = ");
+
+			tr_2.ShowStyle();
+			tr_2.ShowDim();
+
+			Console.WriteLine(" Площадь = " + tr_2.Area());
+			}
 		}
 	}
+*/
+// Добавить конструктор в класс TwoDShape.
+/*
+using System;
+
+namespace Inheritance
+	{
+
+	// Класс для двумерных объектов.
+	class TwoDShape
+		{
+		Double pri_width;
+		Double pri_height;
+
+		// Конструктор класса TwoDShape.
+
+		public TwoDShape(Double w, Double h)
+			{
+			Width = w;
+			Height = h;
+			}
+
+		// Свойства ширины и длины объекта.
+
+		public Double Width
+			{
+			get
+				{
+				return
+					pri_width;
+				}
+			set
+				{
+				pri_width = value < 0 ? -value : value;
+				}
+			}
+		public Double Height
+			{
+			get
+				{
+				return
+					pri_height;
+				}
+			set
+				{
+				pri_height = value < 0 ? -value : value;
+				}
+			}
+		public void ShowDim()
+			{
+			Console.WriteLine(" Ширина и длина = " + Width + " и " + Height);
+			}
+		}
+
+	// Класс для треугольников, производный от класса TwoDShape.
+
+	class Triangle: TwoDShape
+		{
+		String Style;
+		
+		// Вызвать конструктор базового класса.
+
+		public Triangle(String s, Double w, Double h) : base(w, h)
+			{
+			Style = s;
+			}
+
+		// Возвратить площадь треугольника.
+
+		public Double Area()
+			{
+			return
+				Width * Height / 2;
+			}
+
+		// Показать тип треугольника.
+
+		public void ShowStyle()
+			{
+			Console.WriteLine(" Треугольник " + Style);
+			}
+		}
+	class Shapes4
+		{
+		static void Main()
+			{
+			Triangle t1 = new Triangle(" равнобедренный ", 4.0, 4.0);
+			Triangle t2 = new Triangle(" прямоугольный ", 8.0, 12.0);
+
+			Console.WriteLine(" Сведения об объекте t1 = ");
+
+			t1.ShowStyle();
+			t1.ShowDim();
+
+			Console.WriteLine("Площадь = " + t1.Area());
+
+			Console.WriteLine();
+
+			Console.WriteLine(" Сведения об объекте t2 = ");
+
+			t2.ShowStyle();
+			t2.ShowDim();
+
+			Console.WriteLine(" Площадь = " + t2.Area());
+			}
+		}
+	}
+*/
+// Добавить дополнительные конструкторы в класс TwoDShape.
+
+using System;
+
+namespace Inheritance
+	{
+	class TwoDShape
+		{
+		Double pri_width;
+		Double pri_height;
+
+		// Конструктор, вызываемый по умолчанию.
+		public TwoDShape()
+			{
+			Width = Height = 0.0;
+			}
+
+		// Конструктор класса TwoDShape.
+		public TwoDShape(double w, double h)
+			{
+			Width = w;
+			Height = h;
+			}
+		// Сконструировать объект равной ширины и высоты.
+		public TwoDShape(double x)
+			{
+			Width = Height = x;
+			}
+		// Свойства ширины и высоты объекта.
+		public double Width
+			{
+			get
+				{
+				return
+					pri_width;
+				}
+			set
+				{
+				pri_width = value < 0 ? -value : value;
+				}
+			}
+		public double Height
+			{
+			get
+				{
+				return
+					pri_height;
+				}
+			set
+				{
+				pri_height = value < 0 ? -value : value;
+				}
+			}
+		public void ShowDim()
+			{
+			Console.WriteLine("Ширина и высота равны " + Width + " и " + Height);
+			}
+		}
+	// Класс для треугольников, производный от класса TwoDShape.
+	class Triangle: TwoDShape
+		{
+		String Style;
+
+		// Конструктор, используемый по умолчанию.
+		// Автоматически вызывает конструктор, доступный по
+		// умолчанию в классе TwoDShape.
+
+		public Triangle()
+			{
+			Style = "null";
+			}
+		// Конструктор, принимающий три аргумента.
+		public Triangle(String s, Double w, Double h) : base(w, h)
+			{
+			Style = s;
+			}
+		// Сконструировать равнобедренный треугольник.
+		public Triangle(double x) : base(x)
+			{
+			Style = "равнобедренный";
+			}
+		// Возвратить площадь треугольника.
+		public double Area()
+			{
+			return Width * Height / 2;
+			}
+		// Показать тип треугольника.
+		public void ShowStyle()
+			{
+			Console.WriteLine("Треугольник " + Style);
+			}
+		}
+	class Shapes5
+		{
+		static void Main()
+			{
+			Triangle t1 = new Triangle();
+			Triangle t2 = new Triangle("прямоугольный", 8.0, 12.0);
+			Triangle t3 = new Triangle(4.0);
+
+			t1 = t2;
+
+			Console.WriteLine("Сведения об объекте t1: ");
+
+			t1.ShowStyle();
+			t1.ShowDim();
+
+			Console.WriteLine("Площадь равна " + t1.Area());
+
+			Console.WriteLine();
+
+			Console.WriteLine("Сведения об объекте t2: ");
+
+			t2.ShowStyle();
+			t2.ShowDim();
+
+			Console.WriteLine("Площадь равна " + t2.Area());
+
+			Console.WriteLine();
+
+			Console.WriteLine("Сведения об объекте t3: ");
+
+			t3.ShowStyle();
+			t3.ShowDim();
+
+			Console.WriteLine("Площадь равна " + t3.Area());
+
+			Console.WriteLine();
+			}
+		}
+	}
+
+		
